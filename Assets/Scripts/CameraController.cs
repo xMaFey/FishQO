fe491a07c0808fc85f71064872878384704738d8
xMaFey <uhlir2002@seznam.cz>
2024-10-03 11:51:08 +0100

    Camera movement fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,9 @@ public class CameraController : MonoBehaviour {
     public Vector3 offset;
     public bool useOffsetValues;
     public float rotateSpeed;
+    public float maxViewAngle;
+    public float minViewAngle;
+    public bool invertY;
     
 
     // Start is called before the first frame update
@@ -33,7 +36,22 @@ public class CameraController : MonoBehaviour {
 
         //Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        pivot.Rotate(vertical, 0, 0);
+        //pivot.Rotate(vertical, 0, 0);
+
+        if(invertY){
+            pivot.Rotate(vertical, 0, 0);
+        } else {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //Limit up/down camera rotation
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f){
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360 + minViewAngle){
+            pivot.rotation = Quaternion.Euler(360 + minViewAngle, 0, 0);
+        } 
 
         //Move the camera based on the current rotation of the target & the original offset
         float desiredYAngle = target.eulerAngles.y;
